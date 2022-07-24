02a8b69d3c93659861099eff8613232264d58558
Jeanmichel7 <jeanmichel.rasser@laposte.net>
2022-07-24 22:36:01 +0200

    clear

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: jrasser <jrasser@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 22:57:57 by jrasser           #+#    #+#             */
-/*   Updated: 2022/07/24 22:26:27 by jrasser          ###   ########.fr       */
+/*   Updated: 2022/07/24 22:34:29 by jrasser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 #include <limits.h>
 #define FIX_MAX SHRT_MAX
 
+/*
 void printBits(size_t const size, void const *const ptr)
 {
 	unsigned char *b = (unsigned char *)ptr;
@@ -31,6 +32,7 @@ void printBits(size_t const size, void const *const ptr)
 	}
 	puts("");
 }
+*/
 
 int bit_is_one(size_t const size, void const *const ptr, int search_index)
 {
@@ -46,7 +48,6 @@ int bit_is_one(size_t const size, void const *const ptr, int search_index)
 			//printf("test : i : %d, j : %d, char : %u\n", i, j, byte);
 			if (i == 0 && j == search_index && byte == 1)
 				return (1);
-
 		}
 	}
 	return (0);
@@ -66,8 +67,8 @@ Fixed::Fixed(const int cst_int)
 {
 	std::cout << "Int constructor called" << std::endl;
 
-	std::cout << "Int cst:	" << cst_int << std::endl;
-	printBits(sizeof(cst_int), &cst_int);
+	//std::cout << "Int cst:	" << cst_int << std::endl;
+	//printBits(sizeof(cst_int), &cst_int);
 
 	this->setRawBits(cst_int << _nb_fractionnaire);
 	return;
@@ -77,83 +78,21 @@ Fixed::Fixed(const float cst_float)
 {
 	std::cout << "Float construcotr called" << std::endl;
 
-	std::cout << "float :	" << cst_float << std::endl;
-	printBits(sizeof(cst_float), &cst_float);
+	//std::cout << "float :	" << cst_float << std::endl;
+	//printBits(sizeof(cst_float), &cst_float);
 
 	// conv en int
-	float temp = cst_float;
-	int *cast = reinterpret_cast<int *>(&temp);
-	std::cout << "int :	" << *cast << std::endl;
-	printBits(sizeof(*cast), cast);
-
-
+	//float temp = cst_float;
+	//int *cast = reinterpret_cast<int *>(&temp);
+	//std::cout << "int :	" << *cast << std::endl;
+	//printBits(sizeof(*cast), cast);
 
 	int b;
 	b = cst_float * 256;
 	b = roundf(b);
 
-	std::cout << "new int test:	" << b << std::endl;
-	printBits(sizeof(b), &b);
-
-
-/*
-	float new_float;
-	new_float = static_cast<float>(*cast);
-	std::cout << "new_float :	" << new_float << std::endl;
-	printBits(sizeof(new_float), &new_float);
-*/
-
-/*
-	int	cast2 = static_cast<int>(temp);
-	std::cout << "temp2 :	";
-	printBits(sizeof(cast2), &cast2);
-*/
-
-
-/*
-	unsigned sign = x < 0;
-	unsigned absx = ((unsigned)x ^ -sign) + sign; // safe abs(x)
-	unsigned tmp = absx, manbits = 0;
-	int exp = 0, truncated = 0;
-	// calculate the number of bits needed for the mantissa
-	while (tmp)
-	{
-		tmp >>= 1;
-		manbits++;
-	}
-	// half-precision floats have 11 bits in the mantissa.
-	// truncate the excess or insert the lacking 0s until there are 11.
-	if (manbits)
-	{
-		exp = 10; // exp bias because 1.0 is at bit position 10
-		while (manbits > 11)
-		{
-			truncated |= absx & 1;
-			absx >>= 1;
-			manbits--;
-			exp++;
-		}
-		while (manbits < 11)
-		{
-			absx <<= 1;
-			manbits++;
-			exp--;
-		}
-	}
-	if (exp + truncated > 15)
-	{
-		// absx was too big, force it to +/- infinity
-		exp = 31; // special infinity value
-		absx = 0;
-	}
-	else if (manbits)
-	{
-		// normal case, absx > 0
-		exp += 15; // bias the exponent
-	}
-	return (sign << 15) | ((unsigned)exp << 10) | (absx & ((1u << 10) - 1));
-*/
-
+	//std::cout << "new int test:	" << b << std::endl;
+	//printBits(sizeof(b), &b);
 
 	this->setRawBits(b);
 	return;
@@ -181,97 +120,50 @@ Fixed &Fixed::operator=(Fixed &ref_class)
 /* FCT MEMBRE */
 int Fixed::getRawBits(void) const
 {
-	// std::cout << "getRawBits member function called" << std::endl;
 	return (_value);
 }
 
 void Fixed::setRawBits(int const raw)
 {
-	// std::cout << "setRawBits member function called" << std::endl;
 	_value = raw;
 }
 
 float Fixed::toFloat(void) const
 {
-
-/*
-	int new_int;
-	new_int = cst_float / (FIX_MAX+1.0);
-
-	std::cout << "new_int :	" << new_int << std::endl;
-	printBits(sizeof(new_int), &new_int);
-*/
-	float *cast = reinterpret_cast<float *>(getRawBits());
-	return (*cast);
-}
-
-int Fixed::toInt(void) const
-{
-	int cast = getRawBits();
-	return (cast);
-}
-
-/* surcharge opérateur d'insertion */
-std::ostream &operator<<(std::ostream &blabla, const Fixed &instance)
-{
-/*
-	float *new_float;
-
-	new_float = reinterpret_cast<float*>(instance.getRawBits());
-	blabla << &(*new_float);
-*/
 	int 	decimal_value;
 	float	fraction_value = 1;
 	float	virgul_value;
 	int		temp;
 	int		temp2;
 
-	temp = instance.getRawBits();
-	temp2 = instance.getRawBits();
+	temp = getRawBits();
+	temp2 = getRawBits();
 
 	for(int i = 0; i < 8; i++)
-	{
-		//std::cout << "temp : " << temp << std::endl;
-		//printBits(sizeof(temp), &temp);
 		temp >>= 1;
-	}
 	decimal_value = temp;
-	//std::cout << "decimal : " << decimal_value << std::endl;
-	//printBits(sizeof(decimal_value), &decimal_value);
-	//std::cout << std::endl;
-
-
-
 	virgul_value = decimal_value;
-
 	for(int i = 7; i >= 0; i--)
 	{
-		//std::cout << "temp2 : " << temp2 << std::endl;
-		//printBits(sizeof(temp2), &temp2);
-		//std::cout << "virgul value : " << virgul_value << std::endl;
-		//printBits(sizeof(virgul_value), &virgul_value);
-
-
 		fraction_value /= 2;
-
 		if (bit_is_one(sizeof(temp2), &temp2, i))
-		{
 			virgul_value += fraction_value;
-			//printf("il vaux 1 en %d!\n", i);
-		}
-
-
-		//std::cout << std::endl << std::endl;
-
-		
 	}
+	return virgul_value;
+}
 
-	
-	//std::cout << "temp2 : " << temp2 << std::endl;
-	//printBits(sizeof(temp2), &temp2);
-	//std::cout << "virgul value : " << virgul_value << std::endl;
-	//printBits(sizeof(virgul_value), &virgul_value);
+int Fixed::toInt(void) const
+{
+	int		temp;
 
-	blabla << virgul_value;
-	return blabla;
+	temp = getRawBits();
+	for(int i = 0; i < 8; i++)
+		temp >>= 1;
+	return (temp);
+}
+
+std::ostream &operator<<(std::ostream &cout, const Fixed &instance)
+{
+	cout << instance.toFloat();
+	return cout;
 }
--- cpp02/ex01/main.cpp
+++ cpp02/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jrasser <jrasser@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/08 22:58:14 by jrasser           #+#    #+#             */
-/*   Updated: 2022/07/24 22:24:50 by jrasser          ###   ########.fr       */
+/*   Updated: 2022/07/24 22:35:00 by jrasser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,18 @@
 
 int main(void)
 {
-	//Fixed a;
-	//Fixed const b(10);
-	Fixed const c(2.99609375F);
-	//Fixed const d(b);
+	Fixed a;
+	Fixed const b(10);
+	Fixed const c(42.42F);
+	Fixed const d(b);
 	//a = Fixed(1234.4321f);
-	//std::cout << "a is " << a << std::endl;
-	//std::cout << "b is " << b << std::endl;
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
 	std::cout << "c is " << c << std::endl;
-	//std::cout << "d is " << d << std::endl;
-	//std::cout << "a is " << a.toInt() << " as integer" << std::endl;
-	//std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
-	//std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 	return 0;
 }
