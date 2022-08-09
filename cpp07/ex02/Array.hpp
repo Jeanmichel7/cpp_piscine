0acf462213f43092d1b9eceee226e32eeb5092be
Jeanmichel7 <jeanmichel.rasser@laposte.net>
2022-08-09 23:17:52 +0200

    cpp07/ex02

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: jrasser <jrasser@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 05:58:17 by jrasser           #+#    #+#             */
-/*   Updated: 2022/08/09 18:28:14 by jrasser          ###   ########.fr       */
+/*   Updated: 2022/08/09 23:16:27 by jrasser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@
 # include <stdexcept>
 # include <cstdlib>
 
-
 template< typename T >
 class Array
 {
@@ -32,11 +31,10 @@ public:
 	Array();
 	Array(unsigned int n);
 	Array(const Array<T> &);
-	void 		operator=(const Array<T> &);
-	bool 		operator!=(const Array<T> &);
-	//bool 		operator!=(const Array[T]<T> &);
-	Array<T>	&operator[](int i);
 	~Array();
+	void 		operator =  (const Array<T> &);
+	bool 		operator != (T &);
+	T			&operator[] (int i);
 
 	void		display();
 	void		modify();
@@ -59,78 +57,60 @@ template<typename T> Array<T>::Array( void )
 
 template<typename T> Array<T>::Array(unsigned int n)
 {
-	try
-	{
-		std::cout << "Construct Array[" << n << "]" << std::endl;
-		_n = n;
-		_tab = new T[_n];
-		for(unsigned i = 0; i < _n + 1; i++) {
-			if (i >= _n)
-				throw (InvalidIndex());
-			_tab[i] = i + 100;
-		}
+	std::cout << "Construct Array[" << n << "]" << std::endl;
+	_n = n;
+	_tab = new T[_n];
+	for(unsigned i = 0; i < _n; i++) {
+		if (i >= _n)
+			throw (InvalidIndex());
+		_tab[i] = i + 100;
 	}
-	catch(const std::exception& e)
-	{
-		std::cerr << e.what() << '\n';
-	}
+
 }
 
 template< typename T > Array<T>::Array(const Array<T> &tmp)
 {
-	try
-	{
-		std::cout << "Construct Copy Array[" << tmp._n << "]" << std::endl;
+	std::cout << "Construct Copy Array[" << tmp._n << "]" << std::endl;
 
-		//_tab = Array<T>(tmp._n);
-		_n = tmp._n;
-		_tab = new T [tmp._n];
+	_n = tmp._n;
+	_tab = new T [tmp._n];
 
-		for(unsigned i = 0; i < _n; i++) {
-			if (i >= _n)
-				throw (InvalidIndex());
-			_tab[i] = tmp._tab[i];
-		}
-		return;
+	for(unsigned i = 0; i < _n; i++) {
+		if (i >= _n)
+			throw (InvalidIndex());
+		_tab[i] = tmp._tab[i];
 	}
-	catch(const std::exception& e)
-	{
-		std::cerr << "aaaaaaaaaaa!!!!!" << e.what() << '\n';
-	}
+	return;
 }
 
 template< typename T >void  Array<T>::operator=(const Array<T> &tmp){
-	try
-	{
-		std::cout << "Operator affection" << std::endl;
-	
-		delete [] _tab;
-		_n = tmp._n;
-		_tab = new T [tmp._n];
-	
-		for(unsigned i = 0; i < _n; i++) {
-			if (i >= _n)
-				throw (InvalidIndex());
-			_tab[i] = tmp._tab[i];
-		}
-		return;
+	std::cout << "Operator affection" << std::endl;
+
+	delete [] _tab;
+	_n = tmp._n;
+	_tab = new T [tmp._n];
+
+	for(unsigned i = 0; i < _n; i++) {
+		if (i >= _n)
+			throw (InvalidIndex());
+		_tab[i] = tmp._tab[i];
 	}
-	catch(const std::exception& e)
-	{
-		std::cerr << "aaaaaaaaaaa!!!!!" << e.what() << '\n';
-	}
+	return;
 }
 
-template< typename T > bool Array<T>::operator!=(const Array<T> &)
+template< typename T > bool Array<T>::operator!=(T &tmp)
 {
-	return 0;
+	return (*this != tmp ? true : false);
 }
 
+template< typename T > T &Array<T>::operator[](int i)
+{
 
-template< typename T >	Array<T> &Array<T>::operator[](int i){
-	_tab[i] = 10;
+	if (i >= static_cast<int>(_n) || i < 0)
+		throw (InvalidIndex());
+	else
+		return _tab[i];
 }
-
 
 template< typename T > void Array<T>::display() { // try/catch
 	for(unsigned i = 0; i < _n; i++) {
@@ -141,7 +121,7 @@ template< typename T > void Array<T>::display() { // try/catch
 }
 
 template< typename T> void	Array<T>::modify() { // try/catch
-	std::cout << "Modify tab" << std::endl;
+	std::cout << "Modify Array[]" << std::endl;
 	for(unsigned i = 0; i < _n; i++) {
 		_tab[i] += 10;
 	}
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,12 +6,12 @@
 /*   By: jrasser <jrasser@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/09 05:58:41 by jrasser           #+#    #+#             */
-/*   Updated: 2022/08/09 18:11:09 by jrasser          ###   ########.fr       */
+/*   Updated: 2022/08/09 23:17:18 by jrasser          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
-/*
+
 int main()
 {
 	Array<int>	tab1;
@@ -44,62 +44,64 @@ int main()
 	tab2.display();
 	std::cout << "size : " << tab2.size() << std::endl;
 
-
 	return 0;
 }
-*/
 
-
+/*
 #include <iostream>
-//#include <Array.hpp>
+#include <Array.hpp>
 
 #define MAX_VAL 750
 int main(int, char**)
 {
-    Array<int> numbers(MAX_VAL);
-    int* mirror = new int[MAX_VAL];
-    srand(time(NULL));
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        const int value = rand();
-        numbers[i] = value;
-        mirror[i] = value;
-    }
-    //SCOPE
-    {
-        Array<int> tmp = numbers;
-        Array<int> test(tmp);
-    }
+	Array<int> numbers(MAX_VAL);
+	int* mirror = new int[MAX_VAL];
+	srand(time(NULL));
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		const int value = rand();
+		numbers[i] = value;
+		mirror[i] = value;
+	}
+	//SCOPE
+	{
+		Array<int> tmp = numbers;
+		Array<int> test(tmp);
+	}
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        if (mirror[i] != numbers[i])
-        {
-            std::cerr << "didn't save the same value!!" << std::endl;
-            return 1;
-        }
-    }
-    try
-    {
-        numbers[-2] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    try
-    {
-        numbers[MAX_VAL] = 0;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
+	
+	//numbers.display();
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		//std::cout << mirror[i] << " ";
+		if (mirror[i] != numbers[i])
+		{
+			std::cerr << "didn't save the same value!!" << std::endl;
+			return 1;
+		}
+	}
+	try
+	{
+		numbers[-2] = 0;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	try
+	{
+		numbers[MAX_VAL] = 0;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 
-    for (int i = 0; i < MAX_VAL; i++)
-    {
-        numbers[i] = rand();
-    }
-    delete [] mirror;//
-    return 0;
+	for (int i = 0; i < MAX_VAL; i++)
+	{
+		numbers[i] = rand();
+	}
+	delete [] mirror;//
+	return 0;
 }
+*/
